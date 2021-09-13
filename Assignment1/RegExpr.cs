137232da804d496f8a3ab391c0f017ee9ceb31e3
unknown <brma@itu.dk>
2021-09-13 16:24:30 +0200

    Splitline works:)

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,7 +8,16 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach (string line in lines)
+            {
+                var matches = Regex.Matches(line, @"[a-zA-Z0-9]+");
+                foreach (var match in matches)
+                {
+                    yield return match.ToString();
+                }
+            }
+
+        
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
